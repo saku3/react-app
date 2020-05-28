ca8cb92ac94f7c69e87bd9201e9ccd2b4138c3d6
sakurai <39reyskhky@gmail.com>
2020-05-28 21:07:46 +0900

    dynamodbから一覧情報取得

--- backend/todo/app.py
+++ backend/todo/app.py
@@ -1,57 +1,18 @@
+import boto3
 import json
+# from boto3.dynamodb.conditions import Key, Attr
 
-# import requests
+dynamodb = boto3.resource("dynamodb", region_name='ap-northeast-1')
+table = dynamodb.Table('Todo')
 
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-    data = [{
-        'id': 1,
-        'title': 'React',
-        'completed': False
-    },
-        {
-        'id': 2,
-        'title': 'AWS',
-        'completed': False
-    },
-        {
-        'id': 3,
-        'title': 'Terraform',
-        'completed': True
-    }]
+    response = table.scan()
 
     return {
         "statusCode": 200,
         "headers": {
             "Access-Control-Allow-Origin": "*",
         },
-        "body": json.dumps(data),
+        "body": json.dumps(response['Items']),
     }
